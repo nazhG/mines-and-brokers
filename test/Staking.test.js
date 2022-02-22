d282a2625c79e59563d69a73209899ba2f6b4eb2
Nazhir Gonzalez Edbaissi <nazhirgonzalez@gmail.com>
2022-02-22 09:14:46 -0400

    test

--- test/Staking.test.js
+++ test/Staking.test.js
@@ -23,11 +23,13 @@ contract("Staking", async ([manager, ID1, ID2, ID3, ID4]) => {
 		
 		await Bhm.approve(StakingContract.address, toWei("1000000"),{ from: ID1 });
 		
-		console.log("token", Number(await Bhm.balanceOf(ID1)));
+		
+        expect(Number(await Bhm.balanceOf(ID1))).to.be.equal(Number(toWei('200')));
 		await StakingContract.staking(toWei('0.1'),{ from: ID1 });
 		await StakingContract.staking(toWei('0.1'),{ from: ID1 });
+        expect(Number(await Bhm.balanceOf(ID1))).to.be.equal(Number(toWei('199.8')));
 		await StakingContract.withdraw(toWei('0.2'),{ from: ID1 });
-		console.log("token", Number(await Bhm.balanceOf(ID1)));
+        expect(Number(await Bhm.balanceOf(ID1))).to.be.equal(Number(toWei('200')));
 		
 	});
 
@@ -36,41 +38,38 @@ contract("Staking", async ([manager, ID1, ID2, ID3, ID4]) => {
 		await Bhm.approve(StakingContract.address, toWei("1000000"),{ from: ID2 });
 		await Bhm.approve(StakingContract.address, toWei("1000000"),{ from: ID3 });
 		
-		console.log("stake 0", await StakingContract.getStake(ID3));
 		await StakingContract.staking(499 * 1e12,{ from: ID2 });
 		await StakingContract.staking(500 * 1e12,{ from: ID3 });
 		
-		console.log("role 2", Number(await StakingContract.getRole(ID2)));
-		console.log("role 3", Number(await StakingContract.getRole(ID3)));
-		console.log("stake 1", await StakingContract.getStake(ID3));
+        expect(Number(await StakingContract.getRole(ID2))).to.be.equal(0);
+        expect(Number(await StakingContract.getRole(ID3))).to.be.equal(0);
 		await time.increase(time.duration.minutes(30));
 		console.log(await time.latest());
-		console.log("role 2", Number(await StakingContract.getRole(ID2)));
-		console.log("role 3", Number(await StakingContract.getRole(ID3)));
-		console.log("stake 2", await StakingContract.getStake(ID3));
+        expect(Number(await StakingContract.getRole(ID2))).to.be.equal(0);
+        expect(Number(await StakingContract.getRole(ID3))).to.be.equal(1);
 		
 		await StakingContract.staking(1 * 1e12,{ from: ID2 });
 		await StakingContract.withdraw(1 * 1e12,{ from: ID3 });
 		await time.increase(time.duration.minutes(30));
 		
-		console.log("role 2", Number(await StakingContract.getRole(ID2)));
-		console.log("role 3", Number(await StakingContract.getRole(ID3)));
+        expect(Number(await StakingContract.getRole(ID2))).to.be.equal(1);
+        expect(Number(await StakingContract.getRole(ID3))).to.be.equal(0);
 		
 		await StakingContract.staking(4500 * 1e12,{ from: ID2 });
 		await StakingContract.staking(4500 * 1e12,{ from: ID3 });
 		await time.increase(time.duration.minutes(60));
 		
-		console.log("role 2", Number(await StakingContract.getRole(ID2)));
-		console.log("role 3", Number(await StakingContract.getRole(ID3)));
+        expect(Number(await StakingContract.getRole(ID2))).to.be.equal(2);
+        expect(Number(await StakingContract.getRole(ID3))).to.be.equal(1);
 		
 		await StakingContract.withdraw(1 * 1e12,{ from: ID2 });
 		await StakingContract.staking(1 * 1e12,{ from: ID3 });
-		console.log("role 2", Number(await StakingContract.getRole(ID2)));
-		console.log("role 3", Number(await StakingContract.getRole(ID3)));
+        expect(Number(await StakingContract.getRole(ID2))).to.be.equal(1);
+        expect(Number(await StakingContract.getRole(ID3))).to.be.equal(1);
 
 		await time.increase(time.duration.minutes(60));
 		
-		console.log("role 2", Number(await StakingContract.getRole(ID2)));
-		console.log("role 3", Number(await StakingContract.getRole(ID3)));
+        expect(Number(await StakingContract.getRole(ID2))).to.be.equal(1);
+        expect(Number(await StakingContract.getRole(ID3))).to.be.equal(2);
 	});
 });
